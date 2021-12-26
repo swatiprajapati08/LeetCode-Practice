5f3399a05515c91fdf2190f62cd17104c3ed2978
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2021-12-26 11:33:52 +0530

    Time: 66 ms (84.86%), Space: 51.3 MB (74.43%) - LeetHub

---
+++ car-fleet/car-fleet.java
@@ -0,0 +1,40 @@
+class Solution {
+    static class Pair implements Comparable<Pair>{
+        int distance;
+        int speed;
+        double time;
+        
+        Pair(int distance,int speed){
+            this.distance = distance;
+            this.speed = speed;
+            this.time = ((distance * 1.0) / speed);
+        }
+        
+        public int compareTo(Pair other){
+            return this.distance - other.distance;
+        }
+    
+    }
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        
+        Pair[] cars = new Pair[n];
+        
+        for(int i = 0; i<n;i++)
+            cars[i] = new Pair(target - position[i],speed[i]);
+        
+        Arrays.sort(cars);
+        
+        double maxTime =0.0;
+        int fleet = 0;
+        
+        for(int i = 0;i<n;i++){
+            if(cars[i].time > maxTime){
+                maxTime = cars[i].time;
+                fleet++;
+            }
+        }
+        return fleet;
+    }
+}
+
