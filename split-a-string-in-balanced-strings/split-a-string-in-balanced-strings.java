e0e4614f4aef7326c72837cb169dcd249aabf3c9
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2021-12-29 00:27:25 +0530

    Time: 0 ms (100.00%), Space: 36.5 MB (93.57%) - LeetHub

---
+++ split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+      int count = 0;
+        int ans = 0;
+        for(char ch:s.toCharArray()){
+            if(ch == 'L')
+                count++;
+            else
+                count--;
+            if(count == 0)
+                ans++;
+        }
+        return ans;
+    }
+}
