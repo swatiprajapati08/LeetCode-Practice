08142d88dbe94879b11fc7d1b415baae799bbbcf
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2022-01-02 09:59:13 +0530

    Time: 19 ms (79.81%), Space: 42.8 MB (73.85%) - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -1,9 +1,11 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        HashSet<List<Integer>>  ans = new HashSet();
+        ArrayList<List<Integer>>  ans = new ArrayList();
+        //remove duplicate
         Arrays.sort(nums);
         for(int i = 0; i < nums.length - 2;i++){
             int j = i + 1, k = nums.length - 1;
+            if(i > 0 && nums[i] == nums[i - 1]) { continue;}
             while(j < k){
                 int sum = nums[i] + nums[j] + nums[k];
                 if(sum == 0){
@@ -12,15 +14,19 @@ class Solution {
                     res.add(nums[j]);
                     res.add(nums[k]);
                     ans.add(res);
+                    //skip duplicate 
+                    while(j < k && nums[j + 1] == nums[j]) j++;
+                    while(j < k && nums[k - 1] == nums[k]) k--;
+                    
                     j++;
                     k--;
-                }
+                 }
                 else if(sum < 0)
                     j++;
                 else
                     k--;
             }
         }
-        return new ArrayList<List<Integer>>(ans);
+        return ans;
     }
 }
