54b8051f2b70f0e02a01440cdb39acd4f80d8cb5
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2022-01-05 14:05:00 +0530

    Added solution - LeetHub

---
+++ Minimum number of deletions. - GFG/minimum-number-of-deletions..java
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0){
+            int n = Integer.parseInt(read.readLine());
+            String str = read.readLine().strip();
+            Solution ob = new Solution();
+            long ans = ob.minDeletions(str, n); 
+            System.out.println(ans);
+        }
+    } 
+} // } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    int minDeletions(String str, int n)
+    {
+        // code here
+        return n - longestPalindromeSubseq(str);
+    }
+     public int longestPalindromeSubseq(String S) {
+           StringBuilder sb = new StringBuilder(S);
+       return longestCommonSubsequence(S,sb.reverse().toString());
+    }
+      public  int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int dp[][] = new int[m + 1][n + 1];
+        for(int i = 0; i <= m; i++){
+            for(int j = 0; j <= n; j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = 0;
+                }
+                else if(text1.charAt(i - 1) == text2.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else 
+                    dp[i][j] = Math.max(dp[i - 1][j],dp[i][j - 1]);
+            }
+        }
+        return dp[m][n];
+      }
+} 
