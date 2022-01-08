c8640df3e10bd517d7aaf9fd1e13120da59a460e
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2022-01-08 14:57:06 +0530

    Added solution - LeetHub

---
+++ Maximum path sum from any node - GFG/maximum-path-sum-from-any-node.java
@@ -0,0 +1,146 @@
+// { Driver Code Starts
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+                
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root){
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException {
+	        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+	        while(t-- > 0){
+	            String s= br.readLine();
+	            Node root = buildTree(s);
+	            
+	            Solution tr=new Solution();
+	            int sum=tr.findMaxSum(root);
+	            System.out.println(sum);
+	        }
+	}
+}// } Driver Code Ends
+
+
+/*
+Node defined as
+class Node{
+    int data;
+    Node left,right;
+    Node(int d){
+        data=d;
+        left=right=null;
+    }
+}
+*/
+
+class Solution
+{
+    //Function to return maximum path sum from any node in a tree.
+    int findMaxSum(Node node){
+        int sum =Integer.MIN_VALUE ;
+        int array[] = {sum};
+        findMaxSum(node,array);
+        return array[0];
+    }
+    
+    
+    int findMaxSum(Node node,int res[])
+    {
+       if(node == null)
+       return 0;
+       
+       int l = findMaxSum(node.left,res);
+       int r = findMaxSum(node.right,res);
+       
+        l = l < 0 ? 0 : l;
+        r = r < 0 ? 0 : r;
+        // node.data = node.data < 0 ? 0: node.data;
+       
+       int temp = Math.max(Math.max(l,r)+node.data,node.data); // i'm not included
+       int ans = Math.max(l+r+node.data,temp);
+       
+       res[0]= Math.max(res[0], ans);
+       
+       return temp;
+       
+       
+    }
+}
