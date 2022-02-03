c0c15d616b277eb124a5ac20949fed3262a7a5c8
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2022-02-03 23:22:47 +0530

    Added solution - LeetHub

---
+++ 23. Insertion Sort for Singly Linked List - GFG/23.-insertion-sort-for-singly-linked-list.java
@@ -0,0 +1,97 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+class insertion
+{
+    Node head;  
+    Node tail;
+	public void addToTheLast(Node node) 
+	{
+	  if (head == null) 
+	  {
+	   head = node;
+	   tail = node;
+	  } 
+	  else 
+	  {
+	   tail.next = node;
+	   tail = node;
+	  }
+	}
+      void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			insertion llist = new insertion(); 
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+			
+        Solution ob = new Solution();
+		head = ob.insertionSort(llist.head);
+		llist.printList(head);
+		
+        t--;		
+        }
+    }}// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+    */
+class Solution
+{
+    public static Node insertionSort(Node head)
+    {
+           Node curr = head, next = null;
+  // l is a fake head
+  Node l = new Node(0);
+  
+  while (curr != null) {
+    next = curr.next;
+    
+    Node p = l;
+    while (p.next != null && p.next.data < curr.data)
+      p = p.next;
+    
+    // insert curr between p and p.next
+    curr.next = p.next;
+    p.next = curr;
+    curr = next;
+  }
+  
+  return l.next;
+    }
+}
