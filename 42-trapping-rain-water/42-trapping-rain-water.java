e0a14fb662f9166c6b3ed4ca307486c036abd942
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2022-02-07 14:51:13 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (30.01%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -3,15 +3,15 @@ class Solution {
         int n = arr.length;
         int leftMax = 0,rightMax = 0;
         int left = 0, right = n - 1,water = 0;
-        while(left <= right){
-            if(arr[left] <= arr[right]){
-                if(arr[left] < leftMax)  water += (leftMax - arr[left]);
-                else   leftMax = arr[left];
-                    
+        while(left < right){
+            if(arr[left] > leftMax) leftMax = arr[left];
+            if(arr[right] > rightMax) rightMax = arr[right];
+            if(leftMax < rightMax){
+                water += (leftMax - arr[left]);
                 left++;
-            }else{
-                if(arr[right] < rightMax)  water += (rightMax - arr[right]);
-                else   rightMax = arr[right];
+            }
+            else{
+                water += (rightMax - arr[right]);
                 right--;
             }
         }
