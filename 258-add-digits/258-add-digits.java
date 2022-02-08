24fd32ff315fc537eeb3680c91923c8a066ae811
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2022-02-08 09:49:27 +0530

    Time: 2 ms (59.15%), Space: 41.4 MB (9.91%) - LeetHub

--- 258-add-digits/258-add-digits.java
+++ 258-add-digits/258-add-digits.java
@@ -1,13 +1,5 @@
 class Solution {
     public int addDigits(int n) {
-        while(n > 9){
-            int num = n,sum = 0;
-            while(num != 0){
-                sum += num%10;
-                num /= 10;
-            }
-            n = sum;
-        }
-        return n;
+       return n == 0 ? 0 : 1 + (n - 1) % 9;
     }
 }
