b62fd003891b9960bc08d46ede1f7b9932ff4912
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2022-02-19 18:02:16 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (35.05%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -14,37 +14,57 @@
  * }
  */
 class Solution {
-    //or we can use arrayList
-    public List<Integer> rightSideView(TreeNode root) {
-        TreeMap<Integer,Integer> hs = new TreeMap<>();
-        List<Integer> ans = new ArrayList<>();
+//     //or we can use arrayList
+//     public List<Integer> rightSideView(TreeNode root) {
+//         TreeMap<Integer,Integer> hs = new TreeMap<>();
+//         List<Integer> ans = new ArrayList<>();
         
-        DFS(root,0,hs);
-        for(int i:hs.keySet())
-            ans.add(hs.get(i));
+//         DFS(root,0,hs);
+//         for(int i:hs.keySet())
+//             ans.add(hs.get(i));
         
-       return ans;
-    }
+//        return ans;
+//     }
     
+// //     void DFS(TreeNode root,int level,TreeMap<Integer,Integer> hs){
+        
+// //         if(root == null) return;
+// //         if(!hs.containsKey(level))
+// //         hs.put(level,root.val);
+        
+// //         DFS(root.right,level + 1,hs);
+// //         DFS(root.left,level + 1,hs);
+        
+// //     }
+    
+//     // if every time update then call left the right
 //     void DFS(TreeNode root,int level,TreeMap<Integer,Integer> hs){
         
 //         if(root == null) return;
-//         if(!hs.containsKey(level))
+        
 //         hs.put(level,root.val);
         
-//         DFS(root.right,level + 1,hs);
 //         DFS(root.left,level + 1,hs);
-        
+//         DFS(root.right,level + 1,hs);
 //     }
     
-    // if every time update then call left the right
-    void DFS(TreeNode root,int level,TreeMap<Integer,Integer> hs){
+    
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        DFS(root,0,ans);
+       return ans;
+    }
+    
+        void DFS(TreeNode root,int level,List<Integer> ans){
         
         if(root == null) return;
         
-        hs.put(level,root.val);
+        if(level >= ans.size())
+            ans.add(root.val);
+        else ans.set(level,root.val); // if already present then update
         
-        DFS(root.left,level + 1,hs);
-        DFS(root.right,level + 1,hs);
+        DFS(root.left,level + 1,ans);
+        DFS(root.right,level + 1,ans);
     }
+    
 }
