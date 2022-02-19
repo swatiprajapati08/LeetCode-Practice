fb462ff884113ab18686c20a686ecfffae64286f
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2022-02-19 12:03:35 +0530

    Time: 4 ms (55.12%), Space: 47.4 MB (16.76%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
@@ -17,6 +17,32 @@ class Node {
 };
 */
 
+// class Solution {
+//     public List<List<Integer>> levelOrder(Node root) {
+        
+//         List<List<Integer>> res = new ArrayList<>();
+        
+//         if(root == null) return res;
+        
+//         Queue<Node> q = new ArrayDeque<>();
+//         q.add(root);
+        
+//         while(q.size() > 0){
+//             int size = q.size();
+//             List<Integer> ans = new ArrayList<>();
+//             for(int i = 0; i < size;i++){
+//                 Node top = q.remove();
+//                 ans.add(top.val);
+//                 for(int j =0;j< top.children.size();j++)
+//                     q.add(top.children.get(j));
+//             }
+//             res.add(ans);
+//         }
+//         return res;
+//     }
+// }
+
+// Using marker
 class Solution {
     public List<List<Integer>> levelOrder(Node root) {
         
@@ -26,18 +52,25 @@ class Solution {
         
         Queue<Node> q = new ArrayDeque<>();
         q.add(root);
+        Node marker = new Node(-1);
+        q.add(marker);
         
-        while(q.size() > 0){
-            int size = q.size();
-            List<Integer> ans = new ArrayList<>();
-            for(int i = 0; i < size;i++){
-                Node top = q.remove();
-                ans.add(top.val);
-                for(int j =0;j< top.children.size();j++)
-                    q.add(top.children.get(j));
+        List<Integer> level = new ArrayList<>();
+        while(q.size() > 1){  // atlast marker node hi hoga
+            Node curr = q.remove();
+            if(curr == marker){
+                res.add(level);
+                level = new ArrayList<>();
+                q.add(marker);
+            }else{
+                 level.add(curr.val);
+                //pushing its children
+                 for(Node child:curr.children)
+                     q.add(child);
             }
-            res.add(ans);
         }
+        if(level.size() > 0)
+            res.add(level);
         return res;
     }
 }
