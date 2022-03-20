8d6eb5dfa985ca313ae1f8555d5e3cd42c17b2a1
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2022-03-20 09:26:46 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (27.33%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -13,23 +13,45 @@
  *     }
  * }
  */
+
+
 class Solution {
-    public TreeNode bstFromPreorder(int[] preorder) {
-        TreeNode root = new TreeNode(preorder[0]);
-        for(int i = 1;i<preorder.length;i++)
-            add(root,preorder[i]);
-        return root;
-    }
-    //adiing to its root
-    public TreeNode add(TreeNode root, int val){
-        if(root == null)
-            return new TreeNode(val);
+    // O(N2)
+//     public TreeNode bstFromPreorder(int[] preorder) {
+//         TreeNode root = new TreeNode(preorder[0]);
+//         for(int i = 1;i<preorder.length;i++)
+//             add(root,preorder[i]);
+//         return root;
+//     }
+//     //adiing to its root
+//     public TreeNode add(TreeNode root, int val){
+//         if(root == null)
+//             return new TreeNode(val);
         
-        if(root.val > val)
-            root.left = add(root.left,val);
-        else
-            root.right = add(root.right,val);
+//         if(root.val > val)
+//             root.left = add(root.left,val);
+//         else
+//             root.right = add(root.right,val);
         
-        return root;
-    }
+//         return root;
+//     }
+    
+    //USING RANGE O(N)
+    int index = 0;
+     public TreeNode bstFromPreorder(int[] preorder) {
+         return bstCreation(preorder,1,1000);
+     }
+    
+     TreeNode bstCreation(int[] preorder,int leftRange,int rightRange){
+         if(index == preorder.length || leftRange > preorder[index] || rightRange <  preorder[index])
+             return null;
+         
+         TreeNode root = new TreeNode(preorder[index++]);
+         
+         root.left = bstCreation(preorder,leftRange,root.val);
+         root.right = bstCreation(preorder,root.val,rightRange);
+         
+         return root;
+     }
+    
 }
