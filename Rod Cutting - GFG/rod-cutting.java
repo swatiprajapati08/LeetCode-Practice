5b2da0594355591c7e236439fbd6751c985921cd
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2022-04-17 19:45:37 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.java
+++ Rod Cutting - GFG/rod-cutting.java
@@ -24,20 +24,14 @@ class RodCutting {
 
 class Solution{
     public int cutRod(int price[], int n) {
-        int wt[] = new int[n];
-        for(int i =0;i<n;i++)
-        wt[i]= i+1;
-        
-        int dp[][] = new int[n+1][n+1];
-        
-        for(int i = 1;i<=n;i++){
-            for(int j = 1;j<=n;j++){
-                if(wt[i-1]<=j)
-                dp[i][j] = Math.max(dp[i-1][j],dp[i][j-wt[i-1]]+price[i-1]);
-                else
-                dp[i][j] = dp[i-1][j];
-            }
-        }
-        return dp[n][n];
+         int dp[] = new int[n + 1];
+         dp[0] = 0;
+         for(int i=1;i <= n; i++){
+             for(int j = 0;j<i;j++){
+                 if(i - (j + 1) >= 0)
+                 dp[i] = Math.max(dp[i - (j+1)]+ price[j] , dp[i]);
+             }
+         }
+         return dp[n];
     }
 }
