aaccd2944c5c710285b89d12c6b38482bffc921b
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2022-04-23 15:29:10 +0530

    Time: 795 ms (8.25%), Space: 42.7 MB (88.03%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+      int n = nums.length;
+       int dp[] = new int[n];
+       dp[n - 1] = 0; // min moves des to des
+       for(int cell = n - 2; cell >= 0; cell--){
+           int min = 10003;
+           for(int jump = 1; jump <= nums[cell] && jump + cell < n ; jump++)
+               min = Math.min(min,dp[jump + cell] + 1);
+           dp[cell] = min;
+       }
+        return dp[0] == 10003 ? false : true;  
+    }
+}
