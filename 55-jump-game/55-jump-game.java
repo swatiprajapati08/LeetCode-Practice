8460f4d0d829941f1bf27c4a50433c79b53dad87
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2022-04-23 15:43:15 +0530

    Time: 4 ms (43.36%), Space: 67.9 MB (52.18%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,14 +1,25 @@
 class Solution {
-    public boolean canJump(int[] nums) {
-      int n = nums.length;
-       int dp[] = new int[n];
-       dp[n - 1] = 0; // min moves des to des
-       for(int cell = n - 2; cell >= 0; cell--){
-           int min = 10003;
-           for(int jump = 1; jump <= nums[cell] && jump + cell < n ; jump++)
-               min = Math.min(min,dp[jump + cell] + 1);
-           dp[cell] = min;
-       }
-        return dp[0] == 10003 ? false : true;  
+    public boolean canJump(int[] jumps) {
+        //O(N2)
+      // int n = nums.length;
+      //  int dp[] = new int[n];
+      //  dp[n - 1] = 0; // min moves des to des
+      //  for(int cell = n - 2; cell >= 0; cell--){
+      //      int min = 10003;
+      //      for(int jump = 1; jump <= nums[cell] && jump + cell < n ; jump++)
+      //          min = Math.min(min,dp[jump + cell] + 1);
+      //      dp[cell] = min;
+      //  }
+      //   return dp[0] == 10003 ? false : true;  
+
+  //WILL USE GREEDY SOLUTION
+        int reach = 0;
+        for(int i = 0; i<jumps.length;i++){
+            if(i > reach) return false;
+            
+            if(i + jumps[i] > reach)
+                reach = i + jumps[i];
+        }
+        return true;
     }
 }
