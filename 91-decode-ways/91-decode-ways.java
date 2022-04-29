a83f546dd814c5f8d19d4b4e087faf7d139a9916
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2022-04-29 14:29:30 +0530

    Time: 2 ms (61.73%), Space: 42.6 MB (37.93%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,31 +1,19 @@
 class Solution {
     public int numDecodings(String s) {
-        int dp[]= new int[s.length() + 1];
+        int [] dp = new int[s.length() + 1];
         Arrays.fill(dp,-1);
-        return memo(s,0,dp);
+        return memo(dp,0,s);
     }
-    
-    public int memo(String s,int idx,int dp[]){
-        
+    int memo(int[] dp,int idx,String s){
         if(idx == s.length()) return 1;
         
         if(dp[idx] != -1)
             return dp[idx];
         
-        int ans1 = 0,ans2 = 0;
-    
-        if(s.charAt(idx) != '0')
-            ans1 = memo(s,idx + 1,dp);
+        if(s.charAt(idx) == '0') return 0;
         
-        if(idx + 1 < s.length() && s.charAt(idx) != '0'){
-            int code = (s.charAt(idx) - '0') * 10 + (s.charAt(idx + 1) - '0');
-            if(code >= 10 && code <= 26)
-                ans2 = memo(s,idx + 2,dp);
-        }
-        
-        
-        return dp[idx] = ans1 + ans2;
-        
+        int single = memo(dp,idx + 1, s);
+        int pair = idx + 1 < s.length() && Integer.parseInt(s.substring(idx,idx+2)) <= 26 ? memo(dp,idx + 2,s) : 0;
+        return dp[idx] = single + pair;
     }
-    
 }
