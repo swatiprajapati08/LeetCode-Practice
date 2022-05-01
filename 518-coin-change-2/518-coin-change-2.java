2662e07a578e515a822b64a4c90ec72d7aacf145
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2022-05-01 22:43:50 +0530

    Time: 28 ms (13.96%), Space: 52.4 MB (7.90%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -5,21 +5,36 @@ class Solution {
             for(int j = 0; j<coins.length;j++)
                 dp[i][j] = -1;
         }
-        int ans = memo(dp,coins,0,amount);
+        int ans = memo3(dp,coins,amount,0);
         if(ans == Integer.MAX_VALUE) return 0;
         return ans;
         
     }
-     int memo(int dp[][],int coins[],int idx, int amount){
-         if(amount == 0)
-             return 1;
-         if(idx == coins.length) return 0;
-         if(dp[amount][idx] != -1)
-             return dp[amount][idx];
-         int count = 0;
-         for(int i = 0; amount >= (coins[idx] * i);i++)
-             count += memo(dp,coins,idx + 1,amount -(coins[idx] * i));
+     // int memo(int dp[][],int coins[],int idx, int amount){
+//          if(amount == 0)
+//              return 1;
+//          if(idx == coins.length) return 0;
+//          if(dp[amount][idx] != -1)
+//              return dp[amount][idx];
+//          int count = 0;
+//          for(int i = 0; amount >= (coins[idx] * i);i++)
+//              count += memo(dp,coins,idx + 1,amount -(coins[idx] * i));
          
-         return dp[amount][idx] = count;
-     }
+//          return dp[amount][idx] = count;
+//      }
+         // UNBOUNDED  KNAPSACK
+         int memo3(int dp[][],int coins[], int amt, int idx){
+        if(amt == 0) return 1;
+        if(coins.length == idx || amt < 0) return 0;
+        
+        if(dp[amt][idx] != -1)
+            return dp[amt][idx];
+        
+        //int minCoins = Integer.MAX_VALUE - 1 ;
+        int no = memo3(dp,coins,amt,idx + 1);
+        int yes = memo3(dp,coins,amt - coins[idx],idx);
+        
+         return dp[amt][idx] = yes + no;    
+        //return dp[amt][idx] = Math.min(no,yes);
+    }
 }
