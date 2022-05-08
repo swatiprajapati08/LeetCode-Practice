fde0fe10d3d266b9670f9edf5935f9f694a49d31
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2022-05-08 11:40:15 +0530

    Time: 1 ms (89.93%), Space: 42.3 MB (80.91%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,13 +1,39 @@
 class Solution {
     public int maxProfit(int[] prices) {
         //using stack
-        int profit = 0;
-        Stack<Integer> st = new Stack<>();
-        for(int buyDay:prices){
-            if(st.size() > 0 && st.peek() < buyDay)
-                profit += buyDay - st.pop();
-          st.push(buyDay);
+        // int profit = 0;
+        // Stack<Integer> st = new Stack<>();
+        // for(int buyDay:prices){
+        //     if(st.size() > 0 && st.peek() < buyDay)
+        //         profit += buyDay - st.pop();
+        //   st.push(buyDay);
+        // }
+        
+    //     //using extra variable
+    //     int profit = 0;
+    //     int top = 0;
+    //     for(int buyDay:prices){
+    //         if(top <buyDay)
+    //             profit += buyDay -top;
+    //       top =buyDay;
+    //     }
+    // return profit;
+        
+         int buy = -prices[0];
+        int sell = 0;
+        
+        for(int i = 1;i<prices.length;i++){
+            
+            //treating as buying day
+            int newBuy = Math.max(buy,sell - prices[i]);
+            
+            //treating as selling day
+            int newSell = Math.max(sell,buy + prices[i]);
+            
+            sell = newSell;
+            buy = newBuy;
+            
         }
-    return profit;
+        return sell;
     }
 }
