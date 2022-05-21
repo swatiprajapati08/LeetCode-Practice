b5c90e96a84f49d216897d5d0aed165a24babf08
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2022-05-21 10:30:16 +0530

    Time: 75 ms (42.03%), Space: 44.1 MB (63.40%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -3,26 +3,30 @@ class Solution {
         int dp[] = new int[nums.length];
         
         Arrays.fill(dp,1); // no call already taken {1},{2} // single element taken
-        
+        int maxLIS = 1;
         for(int i = 0; i < nums.length;i++){
             // find all its prev index, if prev index element is smaller then current 
             for(int j = 0; j < i; j++){
                 if(nums[j] < nums[i])
                     dp[i] = Math.max(dp[i],dp[j] + 1);
             }
+            maxLIS = Math.max(maxLIS,dp[i]);
         }
         
-        int max = 1;
-        // answer can end any index 
-        for(int i:dp)
-            max = Math.max(i,max);
-        return max;
+        // int max = 1;
+        // // answer can end any index 
+        // for(int i:dp)
+        //     max = Math.max(i,max);
+        // return max;
+        
+        return maxLIS;
     }
 //         int dp[][] = new int[nums.length][nums.length + 1];
 //         for(int[] arr: dp)
 //             Arrays.fill(arr,-1);
 //         return dp(0,-1,nums,dp);
 //     }
+    // -1 ko rkhne k liye we have taken extra 1
 //     int dp(int index, int prev,int[] nums, int dp[][]){
 //         //base case
 //         if(index == nums.length)
