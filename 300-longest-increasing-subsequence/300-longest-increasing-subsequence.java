3bfed40fc7844560394bc8d15fc671cbd560dbd6
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2022-05-21 10:24:02 +0530

    Time: 85 ms (27.75%), Space: 43.7 MB (70.80%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,23 +1,41 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int dp[][] = new int[nums.length][nums.length + 1];
-        for(int[] arr: dp)
-            Arrays.fill(arr,-1);
-        return dp(0,-1,nums,dp);
+        int dp[] = new int[nums.length];
+        
+        Arrays.fill(dp,1); // no call already taken {1},{2} // single element taken
+        
+        for(int i = 0; i < nums.length;i++){
+            // find all its prev index, if prev index element is smaller then current 
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i])
+                    dp[i] = Math.max(dp[i],dp[j] + 1);
+            }
+        }
+        
+        int max = 1;
+        // answer can end any index 
+        for(int i:dp)
+            max = Math.max(i,max);
+        return max;
     }
-    int dp(int index, int prev,int[] nums, int dp[][]){
-        //base case
-        if(index == nums.length)
-            return 0; //length of empty subset 
+//         int dp[][] = new int[nums.length][nums.length + 1];
+//         for(int[] arr: dp)
+//             Arrays.fill(arr,-1);
+//         return dp(0,-1,nums,dp);
+//     }
+//     int dp(int index, int prev,int[] nums, int dp[][]){
+//         //base case
+//         if(index == nums.length)
+//             return 0; //length of empty subset 
         
-        if(dp[index][prev + 1] != -1)
-            return dp[index][prev + 1];
+//         if(dp[index][prev + 1] != -1)
+//             return dp[index][prev + 1];
         
         
-        int yes = (prev == -1 || nums[index] > nums[prev]) ? 
-                dp(index + 1,index,nums,dp) + 1 : 0;
-        int no = dp(index + 1,prev,nums,dp);
+//         int yes = (prev == -1 || nums[index] > nums[prev]) ? 
+//                 dp(index + 1,index,nums,dp) + 1 : 0;
+//         int no = dp(index + 1,prev,nums,dp);
       
-       return dp[index][prev + 1] = Math.max(yes,no);
-    }
+//        return dp[index][prev + 1] = Math.max(yes,no);
+//     }
 }
