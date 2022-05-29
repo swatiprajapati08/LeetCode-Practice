506dd2097d91e2f70a1e97052247c5c617e73f12
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2022-05-29 10:26:43 +0530

    Time: 128 ms (18.25%), Space: 67.9 MB (65.70%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
@@ -1,23 +1,20 @@
 class Solution {
-    public int longestPalindromeSubseq(String S) {
-           StringBuilder sb = new StringBuilder(S);
-       return longestCommonSubsequence(S,sb.reverse().toString());
-    }
-      public  int longestCommonSubsequence(String text1, String text2) {
-        int m = text1.length();
-        int n = text2.length();
-        int dp[][] = new int[m + 1][n + 1];
-        for(int i = 0; i <= m; i++){
-            for(int j = 0; j <= n; j++){
-                if(i == 0 || j == 0){
-                    dp[i][j] = 0;
-                }
-                else if(text1.charAt(i - 1) == text2.charAt(j - 1))
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                else 
-                    dp[i][j] = Math.max(dp[i - 1][j],dp[i][j - 1]);
+    public int longestPalindromeSubseq(String s) {
+      StringBuffer sbr = new StringBuffer(s);
+        // To reverse the string
+        sbr.reverse(); 
+        String t = sbr.toString();
+        
+        int dp[][] = new int[s.length() + 1][t.length() + 1];
+        for(int i = s.length() - 1; i >= 0; i--){
+            for(int j = t.length() - 1; j>=0; j--){
+                if(s.charAt(i) == t.charAt(j))
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                else
+                    dp[i][j] = Math.max(dp[i + 1][j],dp[i][j + 1]);
             }
         }
-        return dp[m][n];
-      }
+        return dp[0][0];
+    }
+    
 }
