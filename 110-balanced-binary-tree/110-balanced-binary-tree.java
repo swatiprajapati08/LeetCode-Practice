cb93b95cc6369ebfa22fcfae3693297c32d7b7bf
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2022-06-02 12:30:44 +0530

    Time: 3 ms (9.22%), Space: 44.9 MB (18.45%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -15,22 +15,54 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if(root == null)
-            return true;
-        int left_h = height(root.left);
-        int right_h = height(root.right);
+//         if(root == null)
+//             return true;
+//         int left_h = height(root.left);
+//         int right_h = height(root.right);
         
-        boolean left = isBalanced(root.left);
-        boolean right = isBalanced(root.right);
+//         boolean left = isBalanced(root.left);
+//         boolean right = isBalanced(root.right);
         
-        if(left == true && right == true && Math.abs(left_h - right_h) <= 1)
-            return true;
-        else
-            return false;
-    }
-    int height(TreeNode root){
-        if(root == null)
-            return 0;
-        return Math.max(height(root.left),height(root.right))+1;
-    }
+//         if(left == true && right == true && Math.abs(left_h - right_h) <= 1)
+//             return true;
+//         else
+//             return false;
+//     }
+//     int height(TreeNode root){
+//         if(root == null)
+//             return 0;
+//         return Math.max(height(root.left),height(root.right))+1;
+//     }
+    
+    // taking n * log n  => log N for calculating its height
+    
+   return Helper(root).isValid;
+    
 }
+static class TreePair{
+    boolean isValid = true;
+    int height = 1;
+}
+TreePair Helper(TreeNode root){
+    
+    if(root == null)
+        return new TreePair();
+    
+    TreePair left = Helper(root.left);
+    TreePair right = Helper(root.right);
+    
+    TreePair curr = new TreePair();
+     curr.height = Math.max(left.height,right.height) + 1;
+    
+    
+    if(left.isValid == true && right.isValid == true && 
+            Math.abs(left.height- right.height) <= 1)
+        curr.height = Math.max(left.height,right.height) + 1;
+    
+    else
+        curr.isValid = false;
+    
+    return curr;
+}
+}
+
